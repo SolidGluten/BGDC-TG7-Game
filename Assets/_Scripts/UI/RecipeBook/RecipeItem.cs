fdf894ee301a4e30f48cf06bb3aebcd557b4b51b
SolidGluten <84835385+SolidGluten@users.noreply.github.com>
2024-03-30 01:35:05 +0700

    Add recipe hovering

--- Assets/_Scripts/UI/RecipeBook/RecipeItem.cs
+++ Assets/_Scripts/UI/RecipeBook/RecipeItem.cs
@@ -2,15 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class RecipeItem : MonoBehaviour
+public class RecipeItem : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public FoodScriptable food;
+    public Image recipeImg;
+    public GameObject textBox;
     public TextMeshProUGUI textMesh;
 
     private void Awake()
     {
+        recipeImg.sprite = food._foodSprite;
         textMesh.text = food.name;
     }
 
+    private void Start()
+    {
+        textBox.SetActive(false);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        textBox.SetActive(true);
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        textBox.SetActive(false);
+    }
 }
